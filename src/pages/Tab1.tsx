4dcafe91977e6f21c82dd30e98440b26e986c7e0
Antoine <bulletforaddicts@icloud.com>
2022-10-09 22:46:41 +0200

    MAJ QUALITY OF CODE
    + Carte tutorat

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -6,104 +6,98 @@ import { Redirect, Route } from 'react-router-dom';
 import { wifi, wine, document, pencil, calculator, telescope, earth } from 'ionicons/icons';
 import Francais from './Francais';
 import Tab2 from './Tab2';
-
 import './Tab1.css';
-
-
-export function Example() {
-  return <IonNav root={() => <Francais />}></IonNav>;
-}
 
 
 const Tab1: React.FC = () => {
   return (
-    <>
-    
-  
-    <Example/>
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
+    <IonPage> // Initialisation de Ionic
+      <IonHeader> // La bannière en haut de l'app
+        <IonToolbar> // ce qui est marqué dans la bannière en haut de l'app
           <IonTitle>Cloud</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonCard button={true} href="https://www.instagram.com/fermat_share/">
-          <IonCardHeader>
+      
+      <IonContent fullscreen> //  mode fullscreen pour une meilleure optimisation
+        // Carte 1 : Notes de version + Compte de l'appli
+        <IonCard button={true} href="https://www.instagram.com/fermat_share/"> //  Url du compte instagram du projet
+          <IonCardHeader> // Tout ce qui concerne titre + Sous-titre
             <IonCardSubtitle>informations</IonCardSubtitle>
             <IonCardTitle>Version Alpha de FermatShare</IonCardTitle>
-            
           </IonCardHeader>
-          <IonCardContent>
-            Vous avez intallé FermatShare ! Bravo. Cette version de FermatShare n'est absolument pas définitive et subira de multiples mise à jour. Si vous rencontrez un bug, n'hésitez pas à le signaler sur le compte instagram de l'appli !
-            (ou en cliquant sur cette carte !)
           
+          <IonCardContent> // Le corps de la carte
+            Vous avez intallé FermatShare ! Bravo. Cette version de FermatShare n'est absolument pas définitive et subira de multiples mises à jour. Si vous rencontrez un bug, n'hésitez pas à le signaler sur le compte instagram de l'appli !
+            (ou en cliquant sur cette carte !)
           </IonCardContent>
-        </IonCard>
+        </IonCard> // Fin Carte 1
+        
+        // Carte 2 : Explication du projet 
         <IonCard>
-          <IonCardHeader>
+          <IonCardHeader> // Tout ce qui concerne titre + Sous-titre
             <IonCardSubtitle>Introduction</IonCardSubtitle>
             <IonCardTitle>Qu'est ce que le cloud ?</IonCardTitle>
           </IonCardHeader>
-
+   
+          <IonCardContent> // Le corps de la carte
+            <a>C'est un espace de partage collaboratif</a>
+            <a>C'est un lieu d'entraide</a>
+            <a>C'est un moyen de s'améliorer</a>
+          </IonCardContent>
+        </IonCard> // Fin Carte 2
         
-          <IonCardContent>
-            <p>C'est un espace de partage collaboratif</p>
-            <p>C'est un lieu d'entraide</p>
-            <p>C'est un moyen de s'améliorer</p>
-            
-            </IonCardContent>
-        </IonCard>
-      <IonCard>          
-        <IonCardHeader>
-          <IonCardTitle>Matières</IonCardTitle>
-          
-            <IonItem button href="./Francais">
-              <IonIcon icon={pencil} slot="start" />
+        // Carte 3 : Le cloud 
+        <IonCard>          
+          <IonCardHeader> // Tout ce qui concerne le titre
+            <IonCardTitle>Matières</IonCardTitle>
+          </IonCardHeader>  
+            // Menu liste des matières du cloud
+              //Français
+              <IonItem button href="./Francais">
+                <IonIcon icon={pencil} slot="start" />
               <IonLabel>Francais</IonLabel>
             </IonItem>
-            
+            // Histoire
             <IonItem href="./Francais">
               <IonIcon icon={document} slot="start" />
               <IonLabel>Histoire</IonLabel>
             </IonItem>
-          
+            // Physique
             <IonItem href="./Francais" >
               <IonIcon icon={telescope} slot="start" />
               <IonLabel>Physique</IonLabel>
             </IonItem>
-          
-          <IonItem href="#">
-            <IonIcon icon={telescope} slot="start" />
-            <IonLabel>Chimie</IonLabel>
-          </IonItem>
-          
-          <IonItem href="#">
-            <IonIcon icon={document} slot="start" />
-            <IonLabel>SES</IonLabel>
-          </IonItem>
-          
-          <IonItem href="#">
-            <IonIcon icon={earth} slot="start" />
-            <IonLabel>ES</IonLabel>
-          </IonItem>
-          
-          <IonItem href="#">
-            <IonIcon icon={document} slot="start" />
-            <IonLabel>Anglais</IonLabel>
-          </IonItem>
-          
-          <IonItem href="#">
-            <IonIcon icon={document} slot="start" />
-            <IonLabel>Espagnol</IonLabel>
-          </IonItem>
-          </IonCardHeader>
+            // Chimie
+            <IonItem href="#">
+              <IonIcon icon={telescope} slot="start" />
+              <IonLabel>Chimie</IonLabel>
+            </IonItem>
+            // SES
+            <IonItem href="#">
+              <IonIcon icon={document} slot="start" />
+              <IonLabel>SES</IonLabel>
+            </IonItem>
+            // ES
+            <IonItem href="#">
+              <IonIcon icon={earth} slot="start" />
+              <IonLabel>ES</IonLabel>
+            </IonItem>
+            // Anglais  
+            <IonItem href="#">
+              <IonIcon icon={document} slot="start" />
+              <IonLabel>Anglais</IonLabel>
+            </IonItem>
+            // Espagnol
+            <IonItem href="#">
+              <IonIcon icon={document} slot="start" />
+              <IonLabel>Espagnol</IonLabel>
+            </IonItem>
+          // Fin du menu liste des matières du cloud  
+        </IonCard> // Fin Carte 3 
         
-        </IonCard>
       </IonContent>
     </IonPage>
-    </>
   );
 };
 
-export default Tab1;
+export default Tab1; // On exporte tout l'onglet 1 en tant que fonction par défaut
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -4,20 +4,21 @@ import './Tab2.css';
 
 const Tab2: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
+    <IonPage> // Initialisation de Ionic
+      <IonHeader>  // La bannière en haut de l'app
+        <IonToolbar> // ce qui est marqué dans la bannière en haut de l'app
           <IonTitle>100% made in Fermât</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+      // début Carte 1 : UNICEF
         <IonCard button={true} href="https://instagram.com/unicef_fermat_?igshid=YmMyMTA2M2Y=">
-          <img src="./pictures/projects/unicef-cover.png" />
-          <IonCardHeader>
+          <img src="./pictures/projects/unicef-cover.png" /> //  Url du compte instagram du projet
+          <IonCardHeader> // Tout ce qui concerne titre + Sous-titre
             <IonCardSubtitle>Lycée Pierre de Fermat +</IonCardSubtitle>
             <IonCardTitle>UNICEF</IonCardTitle>
           </IonCardHeader>
-          <IonCardContent>
+          <IonCardContent> // Le corps de la carte
             Sensibilisation des élèves au sein du lycée (mise en place d'un stand spécial Unicef); 
             Conjonction avec le club écodélégation pour combattre le changement climatique; 
             Vente de roses pour la saint valentin et versement de l'argent collecté à l'Unicef; 
@@ -25,7 +26,20 @@ const Tab2: React.FC = () => {
             Tout autre projet sera retenu ! Que vous soyez membre ou pas ! ;
             Proposez vos idées en tapant (bien fort) sur cette carte !!!!
           </IonCardContent>
-        </IonCard>
+        </IonCard> // fin Carte 1 : UNICEF
+        
+      // début Carte 2 : Tutorat 
+        <IonCard button={true} href="https://www.tutorat-fermat.fr">
+          <img src="./pictures/projects/tutorat-cover.png" /> //  Url du site du projet
+          <IonCardHeader> // Tout ce qui concerne titre + Sous-titre
+            <IonCardSubtitle>Lycée Pierre de Fermat +</IonCardSubtitle>
+            <IonCardTitle>Tutorat</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent> // Le corps de la carte
+            Sans doute une des meilleures idées qu'un élève ait pu avoir... La solidarité. Bref, ce projet vous permet d'être aidé ou d'aider gratuitement d'autre élèves ! Et ce, dans toutes les matières à tous les niveaux (du lycée). Vous pouvez vous inscrire au tutorat en appuyant bien fort sur cette carte !
+          </IonCardContent>
+        </IonCard>  // fin Carte 2 : Tutorat
+        
       </IonContent>
     </IonPage>
   );

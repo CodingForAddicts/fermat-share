b125a21bd41e86f06e99fbe2f08525dc22a47915
Antoine <bulletforaddicts@icloud.com>
2022-10-09 16:30:34 +0200

    Update Francais.tsx

--- src/pages/Francais.tsx
+++ src/pages/Francais.tsx
@@ -14,13 +14,6 @@ import {
 function Francais() {
   return (
 	<>
-	  <IonHeader >
-		<IonToolbar>
-		  <IonButtons slot="start">
-			<IonBackButton></IonBackButton>
-		  </IonButtons>
-		</IonToolbar>
-	  </IonHeader>
 	  <IonContent>
 		<iframe src="https://fermatshare.ddns.net/index.php/s/a9bKAjAFL7kQp3H" width="100%" height="100%" frameBorder="0"></iframe>
 	  </IonContent>

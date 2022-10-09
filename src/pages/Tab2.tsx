d63e7746253cb4885cde78bfe30035cb4979d58f
Antoine <bulletforaddicts@icloud.com>
2022-10-09 23:09:36 +0200

    Update Tab2.tsx

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -5,7 +5,7 @@ import './Tab2.css';
 const Tab2: React.FC = () => {
   return (
     <IonPage> 
-      <IonHeader> 
+      <IonHeader collapse="condense"> 
         <IonToolbar> 
           <IonTitle>100% made in Fermât</IonTitle>
         </IonToolbar>

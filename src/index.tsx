8b755689b873bfe2401f08528869e660ecdd475f
Antoine <bulletforaddicts@icloud.com>
2022-10-10 20:27:08 +0000

    CONNEXION

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ClerkProvider } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
+
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+const frontendApi = process.env.REACT_APP_CLERK_FRONTEND_API;
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   
   <React.StrictMode>
     
-   <>
-   
-    <App/>
-    
-  </>
+    <ClerkProvider frontendApi={frontendApi}>
+   <SignedIn>
+      <App/>
+    </SignedIn>
+    <SignedOut>
+      <RedirectToSignIn />
+    </SignedOut>
+    </ClerkProvider>
     
   </React.StrictMode>
 );
